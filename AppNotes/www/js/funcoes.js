b667d998aad3a27cb1e2f837904616f63f4291c3
SantiagoVinicius <viniciussantiagosantos19@gmail.com>
2022-06-08 21:43:07 -0300

    App Note

---
+++ AppNotes/www/js/funcoes.js
@@ -0,0 +1,12 @@
+function SalvarAnotacao(){
+    novaAnotacao = document.getElementById("anotacao").value
+    document.getElementById("anotacoes-salvas").innerHTML += "<p>" + 
+    novaAnotacao + "</p>"
+    document.getElementById("anotacao").value = " " 
+    alert("anotação salva \ncom sucesso")
+
+}
+
+document.getElementById("botao-salvar").addEventListener("click", function(){
+    SalvarAnotacao()
+})
